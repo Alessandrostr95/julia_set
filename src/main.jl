3e65a711ad8e0343def2e98f4b127ff02e31f5e2
Alessandrostr95 <alessandrostr95@gmail.com>
2022-03-14 01:10:38 +0100

    ripulito il main

--- src/main.jl
+++ src/main.jl
@@ -1,11 +1,13 @@
 const c = -0.79 + 0.15im
+#const c = -0.54 + 0.54im
+#const c = 0.355 + 0.355im
 
 f(z::Complex) = z^2 + c
 
 f(z::Real) = f(z + 0im)
 
-# R > 0 is such that R^2 - R ≥ |c|
-const R = 2
+# R > 0 is such that R√2 ≥ |c|
+const R = 2 
 
 const ε = 0.016
 
@@ -15,13 +17,13 @@ for x=-R:ε:R
   for y=-R:ε:R
     z::Complex = complex(y,x)
 
-    n = 1
-    max_iteration = 100
+    i = 1
+    n = 100
 
-    while abs(z) < R && (n += 1) < max_iteration
+    while abs(z) ≤ √(2)*R && (i += 1) < n
       z = f(z)
     end
-    print(symbols[convert(Int64, ceil(n/11))])
+    print(symbols[convert(Int64, ceil(i/11))])
   end
   println()
 end
